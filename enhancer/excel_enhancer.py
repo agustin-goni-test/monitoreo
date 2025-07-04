2ed981d9ae56f61fe1c1cdd9c97550e349d553d9
Agustín <agustin@MacBook-Pro-de-Agustin.local>
2025-07-04 18:26:42 -0400

    Excel enhancer to add pivot tables for various metrics. Needs more testing.

--- enhancer/excel_enhancer.py
+++ enhancer/excel_enhancer.py
@@ -1,6 +1,16 @@
 import pandas as pd
 from datetime import datetime
 import os
+from openpyxl import load_workbook
+from openpyxl.chart import BarChart, Reference
+from openpyxl.chart.series import SeriesLabel
+from openpyxl.chart.axis import ChartLines
+from openpyxl.chart.label import DataLabelList
+from openpyxl.drawing.text import Font, CharacterProperties
+from openpyxl.workbook import Workbook
+from openpyxl.chart.label import DataLabel
+import openpyxl
+import subprocess
 
 class ExcelEnhancer:
     def __init__(self, file_name):
@@ -79,26 +89,302 @@ class ExcelEnhancer:
         if "Date" not in df.columns:
             raise ValueError("The enhanced file does not contain a 'Date' column. Please run add_context_columns() first.")
         
-        # Find the first column that contains "Time" in its name
-        time_column = next((col for col in df.columns if "Time" in col and "Timestamp" not in col), None)
+        # # Find the first column that contains "Time" in its name
+        # time_column = next((col for col in df.columns if "Time" in col and "Timestamp" not in col), None)
 
-        if not time_column:
-            raise ValueError("No column containing 'Time' found in the enhanced file.")
+        # if not time_column:
+        #     raise ValueError("No column containing 'Time' found in the enhanced file.")
         
-        # Create a pivot table
-        pivot_df = df.groupby("Date", as_index=False)[time_column].mean()
-        pivot_df.rename(columns={time_column: "Average Time"}, inplace=True)
+        # # Create a pivot table
+        # pivot_df = df.groupby("Date", as_index=False)[time_column].mean()
+        # pivot_df.rename(columns={time_column: "Average Time"}, inplace=True)
+
+        # Create result data frame to include all colums
+        result_df = df[['Date']].drop_duplicates().reset_index(drop=True)
+
+        # Find time columns
+        for col in df.columns:
+            if "Time" in col and "Timestamp" not in col and "Compliance" not in col:
+                time_column = col
+                
+                # Rename the column for the target pivot table
+                if "Response Time" in time_column:
+                    new_column_name = col.split("Response Time")[0] + "Average Time (s)"
+                else:
+                    new_column_name = "Average Time (s)"
+
+                # Summarize by date using the mean of the time column
+                dimension_df = df.groupby("Date", as_index=False)[time_column].mean()
+                dimension_df.rename(columns={time_column: new_column_name})
+
+                # Merge with result data frame
+                result_df = result_df.merge(
+                    dimension_df,
+                    on="Date",
+                    how="left"
+                )   
+               
 
         # Write to a new sheet in the enhanced file
         with pd.ExcelWriter(self.enhanced_file_name, engine='openpyxl', mode='a') as writer:
-            pivot_df.to_excel(writer, sheet_name='Time Pivot', index=False)
+            result_df.to_excel(writer, sheet_name='Time Pivot', index=False)
 
         print(f"Pivot table added to {self.enhanced_file_name} in 'Time Pivot' sheet.")
 
 
+    def insert_compliance_pivot(self):
+        if self.enhanced_file_name is None:
+            raise ValueError("Enhanced file name is not set. Call add_context_columns() first.")
+        
+         # Load the enhanced Excel file
+        try:
+            df = pd.read_excel(self.enhanced_file_name)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"The file {self.enhanced_file_name} does not exist.")
+        except Exception as e:
+            raise Exception(f"An error occurred while reading the file: {str(e)}")
+        
+        # Check for Date column
+        if "Date" not in df.columns:
+            raise ValueError("The enhanced file does not contain a 'Date' column. Please run add_context_columns() first.")
+        
+        # Create result data frame to include all colums
+        result_df = df[['Date']].drop_duplicates().reset_index(drop=True)
 
+        for col in df.columns:
+            if "Compliance" in col or "Rate" in col:
+                # Determine which suffix to use
+                suffix = "Compliance (%)" if "Compliance" in col else "Rate (%)"
+
+                # Extract the prefix before either "Response Time" or "Time"
+                if "Response Time" in col:
+                    new_column_name = col.split("Response Time")[0] + suffix
+                elif "Time" in col:
+                    new_column_name = col.split("Time")[0] + suffix
+                else:
+                    new_column_name = col
+
+                # Summarize by date using the mean of the compliance column
+                dimension_df = df.groupby("Date", as_index=False)[col].mean()
+                dimension_df.rename(columns={col: new_column_name}, inplace=True)
+
+                # Merge with result data frame
+                result_df = result_df.merge(
+                    dimension_df,
+                    on="Date",
+                    how="left"
+                )
+
+        # Write to a new sheet in the enhanced file
+        with pd.ExcelWriter(self.enhanced_file_name, engine='openpyxl', mode='a') as writer:
+            result_df.to_excel(writer, sheet_name='Comp Pivot', index=False)
+
+        print(f"Pivot table added to {self.enhanced_file_name} in 'Comp Pivot' sheet.")
+
+
+    def insert_count_pivot(self):
+        if self.enhanced_file_name is None:
+            raise ValueError("Enhanced file name is not set. Call add_context_columns() first.")
         
+         # Load the enhanced Excel file
+        try:
+            df = pd.read_excel(self.enhanced_file_name)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"The file {self.enhanced_file_name} does not exist.")
+        except Exception as e:
+            raise Exception(f"An error occurred while reading the file: {str(e)}")
         
+        # Create result data frame to include all colums
+        result_df = df[['Date']].drop_duplicates().reset_index(drop=True)
+
+        for col in df.columns:
+            if "Count" in col or "Total" in col:
+
+                # Maintain the original column name for clarity, no need for changes   
+
+                # Summarize by date using the sum of the count column
+                dimension_df = df.groupby("Date", as_index=False)[col].sum()
+
+                # Merge with result data frame
+                result_df = result_df.merge(
+                    dimension_df,
+                    on="Date",
+                    how="left"
+                )
+
+        # Write to a new sheet in the enhanced file
+        with pd.ExcelWriter(self.enhanced_file_name, engine='openpyxl', mode='a') as writer:
+            result_df.to_excel(writer, sheet_name='Count Pivot', index=False)
+
+        print(f"Pivot table added to {self.enhanced_file_name} in 'Count Pivot' sheet.")
+
+
+    def create_charts(self):
+        """Create charts based on the pivot DataFrame."""
+
+        # Open the enhanced Excel file, that already has the pivot table
+        # to create charts based on the pivot table data.
+        # Use libraries like openpyxl or xlsxwriter to create charts.
+        wb = load_workbook(self.enhanced_file_name)
+
+        # Verify if the 'Pivot' sheet exists
+        if "Pivot" not in wb.sheetnames:
+            raise ValueError("The 'Pivot' sheet does not exist in the enhanced file.")
+        
+        # Retrieve the pivot sheet
+        pivot_sheet = wb["Pivot"]
+
+        # rows = [
+        #     ('Date', 'Average Time'),
+        #     ('2023-10-01', 12.0),
+        #     ('2023-10-02', 15.0),
+        #     ('2023-10-03', 10.0),
+        #     ('2023-10-04', 20.0),
+        #     ('2023-10-05', 18.0),
+        #     ('2023-10-06', 22.0),
+
+        # ]
+
+        # my_sheet = wb.create_sheet(title="Test Data")
+
+        # for row in rows:
+        #     my_sheet.append(row)
+
+
+        # Create a new sheet for charts
+        chart_sheet = wb.create_sheet(title="Charts")
+
+        # Crete a bar chart
+        chart = BarChart()
+        chart.type = "col" # Column chart (vertical bars)
+        chart.style = 1 # Style of the chart (built-in style)
+        chart.x_axis.majorGridlines = None # No gridlines on the x-axis
+
+        chart.title = "Average Response Time by Date"
+        # chart.x_axis.title = "Date"
+        # chart.y_axis.title = "Average Time (s)"
+        # chart.legend = None # No legend
+
+        # Reference data for the chart. The data is contained in a sheet called 'Pivot'.
+        # In simple words, column A contains the dates and column B contains the average response times.
+        # There is only one row with the column names, so we can use min_row=2 to start from the second row.
+        # There is also only one data series, which contains the average response times by date.
+        values = Reference(pivot_sheet, min_col=2, min_row=1, max_row=pivot_sheet.max_row)
+        categories = Reference(pivot_sheet, min_col=1, min_row=1, max_row=pivot_sheet.max_row)
+
+        # Add data to the chart
+        chart.add_data(values, titles_from_data=True)
+        chart.set_categories(categories)
+   
+        
+
+
+        # Verify we have exactly one series and so series formatting
+        if len(chart.series) == 1:
+            # Get the first (and only) series
+            series = chart.series[0]
+            
+            # This is how you properly modify series properties
+            series.tx = SeriesLabel(v="Average Time")
+            
+            # Ensure consistent formatting
+            series.graphicalProperties.solidFill = "4472C4"  # Standard Excel blue
+
+
+        else:
+            raise ValueError("Chart should have exactly one series")
+
+        # Add the chart to the Charts sheet at cell B2
+        chart_sheet.add_chart(chart, "B2")
+
+        # Save the charts in the enhanced file
+        self.enhanced_file_name = self.enhanced_file_name.replace('.xlsx', '_with_charts.xlsx')
+        wb.save(self.enhanced_file_name)
+        print(f"Charts created and saved in {self.enhanced_file_name} in 'Charts' sheet.")
+
+
+        # # Create bar chart
+        # chart = BarChart()
+        # chart.title = "Average Response Time by Date"
+        # chart.x_axis.title = "Date"
+        # chart.y_axis.title = pivot_sheet.cell(row=1, column=2).value
+
+        # # Reference data
+        # data = Reference(pivot_sheet, min_col=2, min_row=2, max_row=pivot_sheet.max_row)
+        # cats = Reference(pivot_sheet, min_col=1, min_row=2, max_row=pivot_sheet.max_row)
+
+        # chart.add_data(data, titles_from_data=True)
+        # chart.set_categories(cats)
+        
+        # chart.legend = None
+
+        # # Add chart to Charts sheet
+        # chart_sheet.add_chart(chart, "B2")
+
+        # self.enhanced_file_name = self.enhanced_file_name.replace('.xlsx', '_with_charts.xlsx')
+        
+        # # Save the workbook with the new chart
+        # wb.save(self.enhanced_file_name)
+        # print(f"Charts created and saved in {self.enhanced_file_name} in 'Charts' sheet.")
+
+
+    def create_test_chart(self):
+        """Create a test chart with dummy data."""
+        # This method is for testing purposes only.
+        # It creates a chart with dummy data to verify the chart creation functionality.
+        
+        wb = Workbook(write_only=True)
+        ws = wb.create_sheet()
+
+        rows = [
+            ('Number', 'Batch 1', 'Batch 2'),
+            (2, 10, 30),
+            (3, 40, 60),
+            (4, 50, 70),
+            (5, 20, 10),
+            (6, 10, 40),
+            (7, 50, 30),
+        ]
+
+        ws = wb.create_sheet()
+        for row in rows:
+            ws.append(row)
+
+        chart1 = BarChart()
+        chart1.type = "col"
+        chart1.style = 10
+        chart1.title = "Test Chart"
+        chart1.x_axis.title = "Test Number"
+        chart1.y_axis.title = "Test Value"
+
+        data = Reference(ws, min_col=2, min_row=1, max_col=3, max_row=len(rows))
+        cats = Reference(ws, min_col=1, min_row=2, max_row=len(rows))
+
+        chart1.add_data(data, titles_from_data=True)
+        chart1.set_categories(cats)
+        chart1.shape = 4
+
+        chart1.legend = None
+        chart1.height = 20 
+        chart1.width = 30
+
+        chart1.title.tx.rich.p[0].pPr.defRPr = CharacterProperties(sz=2400, b=True)  # Set title font size and bold
+
+        chart1.plot_area.barChart.series[0].cat.strRef.strCache.pt[0].v = "2"  # Set x-axis title
+        
+
+
+
+
+        # Format x-axis title font to be bigger and bold
+        
+
+        # chart1.x_axis.majorGridlines = ChartLines()
+        # chart1.y_axis.majorGridlines = ChartLines() 
+
+        ws.add_chart(chart1, "B10")
+
+        wb.save("output_files/enhanced/test_chart.xlsx") 
 
         
 
@@ -107,7 +393,7 @@ def main():
     print(os.getcwd())
 
     # Create ExcelEnhancer instance
-    enhancer = ExcelEnhancer("output_files/AbonosController_CALC.xlsx")
+    enhancer = ExcelEnhancer("output_files/ComercioTransaccionesController_DEFAULT.xlsx")
 
     # Add context columns to the Excel file
     try:
@@ -115,9 +401,64 @@ def main():
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
-    enhancer.set_file_name("output_files/enhanced/AbonosController_CALC_ENH.xlsx")
+    enhancer.set_file_name("output_files/enhanced/ComercioTransaccionesController_DEFAULT_ENH.xlsx")
 
+    # Insert pivot datsable based on time context columns
     enhancer.insert_time_pivot()
+
+    # Insert pivot datsable based on compliance context columns
+    enhancer.insert_compliance_pivot()
+
+    # Insert pivot datsable based on count context columns
+    enhancer.insert_count_pivot()
+
+    # Insert chart for the pivot table (in a separete sheet
+    # enhancer.create_test_chart()
+
+    # Take enhanced file name and add "with_charts" suffix
+    # excel_file = enhancer.enhanced_file_name
+
+    # print(f"Source file for the creation of files: {excel_file}")
+
+    # charts_dll = r"/Users/agustin/Documents/C#/ExcelChartTest/bin/Release/net9.0/ExcelChartTest.dll"
+
+    # if not os.path.exists(charts_dll):
+    #     raise FileNotFoundError(f"DLL not found at: {charts_dll}")
+    # if not os.path.exists(excel_file):
+    #     raise FileNotFoundError(f"Excel file not found at: {excel_file}")
+    
+    # dotnet_path = "/usr/local/share/dotnet/dotnet"
+
+    # command = [dotnet_path, 
+    #            "exec",
+    #            charts_dll,
+    #            excel_file
+    #            ]
+
+    # print(f"Running command: {' '.join(command)}")
+
+    # try:
+    #     result = subprocess.run(
+    #         command,
+    #         check=True,
+    #         capture_output=True,
+    #         text=True
+    #         )
+
+    #     print("Ran command successfully...")
+    #     print("Output:", result.stdout)
+    #     print(f"Charts created and saved in {excel_file} in 'Charts' sheet.")
+
+    # except subprocess.CalledProcessError as e:
+    #     print(f"Error running command: {e}")
+    #     print("Output:", e.stdout)
+    #     print("Error Output:", e.stderr)
+    #     raise
+
+    
+
+
+    
 
     print("The end")
 
